1789a28379f301d898d97c5feeaeaad727f3c4eb
Hein Dauven <heindauven@gmail.com>
2017-06-01 10:24:23 +0200

    Added small exception handlding methods for REST based error messaging

--- src/main/java/com/kwetter/exception/DataNotFoundException.java
+++ src/main/java/com/kwetter/exception/DataNotFoundException.java
@@ -3,5 +3,9 @@ package com.kwetter.exception;
 /**
  * Created by hein on 4/3/17.
  */
-public class DataNotFoundException {
+public class DataNotFoundException extends RuntimeException {
+
+    public DataNotFoundException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/kwetter/exception/DataNotFoundExceptionMapper.java
+++ src/main/java/com/kwetter/exception/DataNotFoundExceptionMapper.java
@@ -1,7 +1,22 @@
 package com.kwetter.exception;
 
+import com.kwetter.model.ErrorMessage;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
 /**
  * Created by hein on 4/3/17.
  */
-public class DataNotFoundExceptionMapper {
+@Provider
+public class DataNotFoundExceptionMapper implements ExceptionMapper<DataNotFoundException> {
+    @Override
+    public Response toResponse(DataNotFoundException exception) {
+        ErrorMessage errorMessage = new ErrorMessage(exception.getMessage(), 404, "");
+        return Response.status(Status.NOT_FOUND)
+                .entity(errorMessage)
+                .build();
+    }
 }
